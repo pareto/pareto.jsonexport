7f17173b4feb77cea94a8286746db7aae100128a
johnny <guido.wesdorp@pareto.nl>
2013-07-12 16:21:20 +0200

    Meh, copy/paste issues.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.4'
+version = '0.1'
 
 long_description = (
     open('README.txt').read()
@@ -47,5 +47,5 @@ setup(name='pareto.jsonexport',
       [z3c.autoinclude.plugin]
       target = plone
       """,
-      setup_requires=["PasteScript"],
+      setup_requires=[],
       )
